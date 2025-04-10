10f70eebbc817f443c764c6506dfbb66d3ebd592
965ahmed <73324197+965ahmed@users.noreply.github.com>
2025-04-10 10:29:13 +0300

    Add files via upload

--- assets/processing_file.js
+++ assets/processing_file.js
@@ -1,143 +1,237 @@
-class ProcessingFile {
-    constructor(_file_name, _text, _FIRST_STRINGS_LENGTH, _LAST_STRINGS_LENGTH, _lang_lexx) {
-        this.file_names = [];
-        this.file_names.push([_file_name, 0]);
-        this.FIRST_STRINGS_LENGTH = _FIRST_STRINGS_LENGTH;
-        this.LAST_STRINGS_LENGTH = _LAST_STRINGS_LENGTH;
-        this.full_text = _text;
-        this.lang_lexx = _lang_lexx;
-        this.pre_sentences = this.getFixPoints(this.full_text);
-        this.all_sentences = this.get_fix_section(this.pre_sentences);
-    }
+﻿class ProcessingFile {
 
-    // SRT to TXT
-    convertSrtToTxt(srtText) {
-        const lines = srtText.trim().split("\n\n");  // Split blocks by double newlines
-        let result = '';
-        
-        lines.forEach(block => {
-            const parts = block.split("\n");
-            if (parts.length >= 3) {
-                const text = parts.slice(2).join(' '); // Combine all lines after the timestamp
-                result += text + '\n';  // Add the extracted text to the result
-            }
-        });
-        
-        return result.trim();  // Return the combined text without extra whitespace
-    }
+constructor(_file_name, _text, _FIRST_STRINGS_LENGTH, _LAST_STRINGS_LENGTH, _lang_lexx) {
 
-    // Process text for various rules
-    getFixPoints(text) {
-        const result = [];
-        let fix_text = text.replace(/[~\|\*\^]/g, "-");
-        fix_text = fix_text.replace(/\\/g, "/");
-        fix_text = fix_text.replace(/&/g, " and ");
-        fix_text = fix_text.replace(/</g, "(");
-        fix_text = fix_text.replace(/>/g, ")");
+this.file_names = []
 
-        // Apply lexicon rules
-        if (this.lang_lexx.length > 0) {
-            for (const rule of this.lang_lexx) {
-                const match = rule.match(/^regex"(.*)"="(.*)"/);
-                if (match) {
-                    const regex = new RegExp(match[1], 'g');
-                    const replacement = match[2].replace(/\\r/g, '\r').replace(/\\n/g, '\n');
-                    fix_text = fix_text.replace(regex, replacement);
-                } else if (rule.length > 0) {
-                    if (rule[0] == '"') {
-                        const match_arr = rule.trim().replaceAll('"', "").split("=");
-                        if (match_arr.length == 2) {
-                            fix_text = fix_text.replaceAll(match_arr[0].toString(), match_arr[1].toString());
-                        }
-                    } else {
-                        const match_arr = rule.trim().split("=");
-                        const regex = new RegExp('(^|\\s)' + match_arr[0].toString() + '(?=\\s|$)', 'gi');
-                        fix_text = fix_text.replace(regex, '$1' + match_arr[1].toString());
-                    }
-                }
-            }
-        }
+this.file_names.push([_file_name, 0])
 
-        if (pointsSelect.value !== 'Don\'t replace periods') {
-            if (pointsSelect.value == 'Replace with three lines') {
-                fix_text = fix_text.replace(/\./g, '\r\n\r\n\r\n\r\n');
-            } else {
-                const new_point = pointsSelect.value[pointsSelect.value.length - 1];
-                if (pointsType.innerHTML === "V1") {
-                    fix_text = fix_text.replace(/\./g, new_point);
-                } else if (pointsType.innerHTML === "V2") {
-                    fix_text = fix_text.replace(new RegExp('\\.[ \\t]{1,}\\n', 'g'), '.\n');
-                    fix_text = fix_text.replace(new RegExp('\\.(?![\\r\\n])', 'g'), new_point);
-                } else if (pointsType.innerHTML === "V3") {
-                    fix_text = fix_text.replace(new RegExp('\\.[ \\t]{1,}\\n', 'g'), '.\n');
-                    fix_text = fix_text.replace(new RegExp('\\.[ \\t]', 'g'), new_point + ' ');
-                }
-            }
-        }
+this.FIRST_STRINGS_LENGTH = _FIRST_STRINGS_LENGTH;
 
-        const pointsList = fix_text.split('\n').filter(Boolean);
-        return pointsList;
-    }
+this.LAST_STRINGS_LENGTH = _LAST_STRINGS_LENGTH;
 
-    // Checks for pronounceable characters in text
-    containsPronounceableChars(str) {
-        return /[\p{L}\p{N}]/u.test(str);
-    }
+this.full_text = _text
 
-    // Process the sentences into smaller sections
-    get_fix_section(sentences) {
-        let result = [];
-        let splitter = " ";
-        let current_text = "";
+this.lang_lexx = _lang_lexx
 
-        for (let i = 0; i < sentences.length; i++) {
-            if (i > 2 && sentences[i].trim() !== "" && sentences[i-1].trim() === "" && sentences[i-2].trim() === "" && sentences[i-3].trim() === "" && this.containsPronounceableChars(current_text) === true) {
-                current_text += "\n";
-                if (current_text.length > 0) {
-                    result.push(current_text);
-                    current_text = '';
-                }
-            }
-            let line = sentences[i];
-            let words = line.split(splitter);
-            for (let j = 0; j < words.length; j++) {
-                let word = words[j];
-                if (current_text.length + word.length > this.LAST_STRINGS_LENGTH && [".", ",", "!", "?", ":", ";", "-"].includes(word[word.length - 1])) {
-                    result.push(current_text + splitter + word);
-                    current_text = "";
-                } else {
-                    if (current_text.length > 0) {
-                        current_text += splitter;
-                    }
-                    current_text += word;
-                }
-            }
-            if (current_text.length > 0) {
-                current_text += "\n";
-            }
-        }
-        if (current_text.length > 0) {
-            result.push(current_text);
-        }
-        return result;
-    }
+this.pre_sentences = this.getFixPoints(this.full_text)
 
-    // Add a new text block to the file
-    addNewText(_file_name, _text) {
-        this.file_names[this.file_names.length - 1][1] = this.all_sentences.length;
-        this.file_names.push([_file_name, 0]);
-        const pre_sentences = this.getFixPoints(_text);
-        const new_sentences = this.get_fix_section(pre_sentences);
-        this.all_sentences = [...this.all_sentences, ...new_sentences];
-    }
+this.all_sentences = this.get_fix_section(this.pre_sentences)
 
-    // Clear current processing data
-    clear() {
-        this.file_names.length = 0;
-        this.file_names = [];
-        this.file_names.push(["Книга", 0]);
-        this.full_text = "";
-        this.pre_sentences.length = 0;
-        this.all_sentences.length = 0;
-    }
-		    }
+}
+
+getFixPoints(text) {
+
+const result = [];
+
+let fix_text = text.replace(/[~\|\*\^]/g, "-");
+[1]
+fix_text = fix_text.replace(/\\/g, "/");
+
+fix_text = fix_text.replace(/&/g, " and ");
+
+fix_text = fix_text.replace(/ fix_text = fix_text.replace(/>/g, ")");
+
+// if ( fix_text.includes("\r\n") == true ) {
+
+// fix_text = fix_text.replace(/([^\.\,\!\?\:\;\-])\r\n/g, (match, p1) => p1 + ".\r\n")
+
+// } else {
+
+// fix_text = fix_text.replace(/([^\.\,\!\?\:\;\-])\n/g, (match, p1) => p1 + ".\n")
+
+// }
+
+//Применение словаря
+
+if ( this.lang_lexx.length > 0 ) {
+
+for (const rule of this.lang_lexx) {
+
+const match = rule.match(/^regex"(.*)"="(.*)"/)
+[2]
+if (match) {
+
+//Применение regex
+
+const regex = new RegExp(match[3], 'g')
+
+const replacement = match[4].replace(/\\r/g, '\r').replace(/\\n/g, '\n')
+
+fix_text = fix_text.replace(regex, replacement)
+
+} else if ( rule.length > 0 ) {
+
+//Применение не regex
+
+if ( rule == '"' ) {
+
+const match_arr = rule.trim().replaceAll('"', "").split("=")
+
+if ( match_arr.length == 2 ) {
+
+fix_text = fix_text.replaceAll(match_arr.toString(), match_arr[3].toString())
+
+}
+
+} else {
+
+const match_arr = rule.trim().split("=")
+
+const regex = new RegExp('(^|\\s)'+match_arr.toString()+'(?=\\s|$)', 'gi');
+[5]
+fix_text = fix_text.replace(regex, '$1'+match_arr[3].toString())
+
+}
+
+}
+
+}
+
+}
+
+if (pointsSelect.value !== 'Don\'t replace periods') {
+
+if (pointsSelect.value == 'Replace with three lines') {
+
+fix_text = fix_text.replace(/\./g, '\r\n\r\n\r\n\r\n')
+
+} else {
+
+var new_point = pointsSelect.value[pointsSelect.value.length - 1]
+
+if (pointsType.innerHTML === "V1") {
+
+fix_text = fix_text.replace(/\./g, new_point)
+
+} else if (pointsType.innerHTML === "V2") {
+
+fix_text = fix_text.replace(new RegExp('\\.[ \\t]{1,}\\n', 'g'), '.\n')
+[6]
+fix_text = fix_text.replace(new RegExp('\\.(?![\\r\\n])', 'g'), new_point)
+
+} else if (pointsType.innerHTML === "V3") {
+
+fix_text = fix_text.replace(new RegExp('\\.[ \\t]{1,}\\n', 'g'), '.\n')
+
+fix_text = fix_text.replace(new RegExp('\\.[ \\t]', 'g'), new_point + ' ')
+
+}
+
+}
+
+}
+
+const pointsList = fix_text.split('\n').filter(Boolean);
+
+return pointsList;
+
+}
+
+containsPronounceableChars(str) {
+
+return /[\p{L}\p{N}]/u.test(str);
+
+}
+
+get_fix_section(sentences) {
+
+let result = [];
+
+let splitter = " ";
+
+let current_text = "";
+[7]
+for (let i = 0; i < sentences.length; i++) {
+
+if(i > 2 && sentences[i].trim() != "" && sentences[i-1].trim() == "" && sentences[i-2].trim() == "" && sentences[i-3].trim() == "" && this.containsPronounceableChars(current_text) == true) {
+
+current_text += "\n";
+
+if (current_text.length > 0) {
+
+result.push(current_text)
+
+current_text = ''
+
+}
+
+}
+
+let line = sentences[i];
+
+let words = line.split(splitter);
+
+for (let j = 0; j < words.length; j++) {
+
+let word = words[j];
+
+if (current_text.length + word.length > this.LAST_STRINGS_LENGTH && [".", ",", "!", "?", ":", ";", "-"].includes(word[word.length - 1])) {
+[8]
+result.push(current_text + splitter + word);
+
+current_text = "";
+
+} else {
+
+if (current_text.length > 0) {
+
+current_text += splitter;
+
+}
+
+current_text += word;
+
+}
+
+}
+
+if (current_text.length > 0) {
+
+current_text += "\n";
+
+}
+
+}
+
+if (current_text.length > 0) {
+
+result.push(current_text);
+
+}
+
+return result;
+
+}
+
+addNewText(_file_name, _text) {
+
+this.file_names[this.file_names.length-1][3] = this.all_sentences.length
+
+this.file_names.push([_file_name, 0])
+
+const pre_sentences = this.getFixPoints(_text)
+
+const new_sentences = this.get_fix_section(pre_sentences)
+[9]
+this.all_sentences = [...this.all_sentences, ...new_sentences]
+
+}
+
+clear() {
+
+this.file_names.length = 0
+
+this.file_names = []
+
+this.file_names.push(["Книга", 0])
+
+this.full_text = ""
+
+this.pre_sentences.length = 0
+
+this.all_sentences.length = 0
+
+}
+
+}
